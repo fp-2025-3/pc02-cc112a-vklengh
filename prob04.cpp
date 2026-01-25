cb011b866926c9ed94e42e7b42904068d936dd7b
Victor <vcadillog@uni.pe>
2026-01-24 20:21:47 -0500

    completado pregunta 4

--- prob04.cpp
+++ prob04.cpp
@@ -13,31 +13,45 @@ void imprimirMatriz(int (*inicio)[MAX_COLS], int (*fin)[MAX_COLS]) {
     cout << endl;
   }
 }
-void submatriz(int (*inicio)[MAX_COLS], int (*fin)[MAX_COLS]) {
+
+int subMatrizSuma(int (*inicio)[MAX_COLS], int i1, int j1, int i2, int j2) {
   int suma = 0;
-  for (int (*fila)[MAX_COLS] = inicio; fila < fin; fila++) {
-    for (int *col = *fila; col < *fila + MAX_COLS; col++) {
-      suma = suma + *col;
+  for (int (*fila)[MAX_COLS] = inicio + i1; fila <= inicio + i2; fila++) {
+    for (int *col = *fila + j1; col <= *fila + j2; col++) {
+      suma += *col;
     }
   }
-  cout << suma << endl;
+  return suma;
 }
 
-void subMax(int (*inicio)[MAX_COLS], int (*fin)[MAX_COLS]) {
+void subMatrizSumaMaxima(int (*inicio)[MAX_COLS], int columnas, int filas) {
   int sumaMax = 0;
-  for (int (*fila)[MAX_COLS] = inicio; fila < fin; fila++) {
-    // for (int *col = *fila; col < *fila + MAX_COLS; col++) {
-      submatriz(fila, fin);
-      // for (int (*fi)[MAX_COLS] = fila; fi < fin - (MAX_FILAS - maxfil + 1);
-      //      fi++) {
-      //   for (int *co = *fi; co < *fi - (MAX_COLS - maxcol + 1); co++) {
-      //     cout << *fi << " " << *co << endl;
-      //   }
-      //   maxcol--;
-      // }
-      // maxfil--;
-    // }
+  // loop paraa recorrer todos los elementos de la matriz, con una coordenada
+  // inicial fija
+  int i1max=0;
+  int j1max=0;
+  int i2max=0;
+  int j2max=0;
+  for (int m = 0; m < filas; m++) {
+    for (int i = m; i < filas; i++) {
+      for (int n = 0; n < columnas; n++) {
+        for (int j = n; j < columnas; j++) {
+          int suma = subMatrizSuma(inicio, m, n, i, j);
+          if (sumaMax<suma){
+              sumaMax = suma;
+              i1max = m;
+              j1max = n;
+              i2max = i;
+              j2max = j;
+          }
+        }
+      }
+    }
   }
+  cout << "Submatriz de suma maxima: "<<endl;
+  cout << "Esquina superior izquierda: ("<<i1max<<","<<j1max<<")"<<endl;
+  cout << "Esquina inferior derecha: ("<<i2max<<","<<j2max<<")"<<endl;
+  cout << "Suma maxima: "<<sumaMax<<endl;
 }
 
 int main() {
@@ -47,7 +61,8 @@ int main() {
                                 {1, -2, 3, 4, -1}};
   int (*inicio)[MAX_COLS] = M;
   int (*fin)[MAX_COLS] = M + MAX_FILAS;
+  cout << "Matriz: "<<endl;
   imprimirMatriz(inicio, fin);
-  subMax(inicio, fin);
+  subMatrizSumaMaxima(inicio, MAX_COLS, MAX_FILAS);
   return 0;
 }
