3becfd937f78f9600da44fd2c250f8ae54eb16a9
Victor Cadillo <vklengh>
2026-02-09 05:15:25 +0000

    problema 4 terminado

--- semana04/parcial/prob04.cpp
+++ semana04/parcial/prob04.cpp
@@ -1,56 +1,129 @@
 #include <iostream>
 using namespace std;
 
-int *crearArreglo(int n, char *texto) {
-  int *array = new int[n];
-  for (int i = 0; i < n; i++) {
-    cout << texto << " : ";
-    cin >> array[i];
-  }
-  return array;
-}
+const char texto1[] = "Codigo";
+const char texto2[] = "Promedio";
 
-int *AumentarArreglo(int *arr, int tam, int n, char *texto) {
-  if (tam == n) {
-    int *temp = new int[2 * n];
-    for (int i = 0; i < n; i++) {
-      temp[i] = arr[i];
+void inicializar_estudiantes(int *&codigo, float *&promedio, int n, int inicial) {
+  int *temp_cod = new int[n];
+  float *temp_prom = new float[n];
+  for (int i = 0; i < n; i++) {
+    if (i < inicial) {
+      cout << texto1 << " : ";
+      cin >> temp_cod[i];
+      cout << texto2 << " : ";
+      cin >> temp_prom[i];
     }
-    cout << texto << " : ";
-    cin >> arr[n + 1];
-    delete[] arr;
-    return temp;
+  }
+  codigo = temp_cod;
+  promedio = temp_prom;
+}
+
+void agregar_estudiantes(int *&codigo, float *&promedio, int &n, int inicial,
+                         int adicional) {
+  if (inicial + adicional >= n) {
+    n *= 2;
+    int *temp_cod = new int[n];
+    float *temp_prom = new float[n];
+    for (int i = 0; i < inicial; i++) {
+      temp_cod[i] = codigo[i];
+      temp_prom[i] = promedio[i];
+    }
+    for (int i = inicial; i < inicial + adicional; i++) {
+      cout << texto1 << " : ";
+      cin >> temp_cod[i];
+      cout << texto2 << " : ";
+      cin >> temp_prom[i];
+    }
+    delete[] codigo;
+    delete[] promedio;
+    codigo = nullptr;
+    promedio = nullptr;
+    codigo = temp_cod;
+    promedio = temp_prom;
   } else {
-    cout << texto << " : ";
-    cin >> arr[tam + 1];
-    return arr;
+    for (int i = inicial; i < inicial + adicional; i++) {
+      cout << texto1 << " : ";
+      cin >> codigo[i];
+      cout << texto2 << " : ";
+      cin >> promedio[i];
+    }
   }
 }
-
-void EliminarMemoria(int *&arr) {
-  delete[] arr;
-  arr = nullptr;
+void EliminarMemoria(int *&codigo, float *&promedio) {
+  delete[] codigo;
+  delete[] promedio;
+  codigo= nullptr;
+  promedio=nullptr;
 }
 
-void eliminarEstudiantes(int *codigo, int *promedio, int n) {
-  for (int i = 0; i < n; i++) {
-      if (promedio[i]<10){}
+int eliminarEstudiantes(int *&codigo, float *&promedio, int final) {
+  int contador = 0;
+  // Un bucle para determinar la cantidad de elementos que el
+  // array debe tener y otro para hacer las asignaciones
+  for (int i = 0; i < final; i++) {
+    if (promedio[i] >= 10) {
+      contador++;
+    }
+  }
+  int *temp_cod = new int[contador];
+  float *temp_prom = new float[contador];
+  int idx = 0;
+  for (int i = 0; i < final; i++) {
+    if (promedio[i] >= 10) {
+      temp_cod[idx] = codigo[i];
+      temp_prom[idx] = promedio[i];
+      idx++;
+    }
+  }
+  delete[] codigo;
+  delete[] promedio;
+  codigo = nullptr;
+  promedio = nullptr;
+  codigo = temp_cod;
+  promedio = temp_prom;
+  return contador;
+}
+
+void imprimir(int *codigo, float *promedio, int inicial, int adicional) {
+  for (int i = 0; i < inicial + adicional; i++) {
+    cout << "Codigo: " << codigo[i] << " Promedio: " << promedio[i] << endl;
   }
 }
 
 int main() {
-  int n;
-  cout << "Ingrese la cantidad de estudiantes: ";
+  int n; // tamano de arreglo
+  int n_inicial, n_extra;
+  int n1,n2,nf;
+  cout << "Ingrese el tamanio del arreglo: ";
   cin >> n;
-  char texto1[] = "Codigo";
-  char texto2[] = "Promedio";
-  int *codigo = crearArreglo(n, texto1);
-  int *promedio = crearArreglo(n, texto2);
+  cout << "Ingrese la cantidad de estudiantes a registrar: ";
+  cin >> n_inicial;
+  int *codigo;
+  float *promedio;
+  n1 = n;
+  inicializar_estudiantes(codigo, promedio, n, n_inicial);
 
-  int tam = sizeof(codigo) / sizeof(codigo[0]);
-  codigo = AumentarArreglo(codigo, tam, n, texto1);
-  promedio = AumentarArreglo(promedio, tam, n, texto2);
-  EliminarMemoria(codigo);
-  EliminarMemoria(promedio);
+  imprimir(codigo, promedio, n_inicial, 0);
+
+  cout << "Ingrese la cantidad de estudiantes que quiere agregar: ";
+  cin >> n_extra;
+
+  agregar_estudiantes(codigo, promedio, n, n_inicial, n_extra);
+  n2 = n;
+  cout << "Estudiantes registrados:"<<endl; 
+  imprimir(codigo, promedio, n_inicial, n_extra);
+  n = eliminarEstudiantes(codigo, promedio, n_inicial + n_extra);
+  nf = n;
+  cout <<"\n"<<endl;
+  cout << "Filtrando a estudiantes desaprobados..."<<endl;
+  cout <<"\n"<<endl;
+  imprimir(codigo, promedio, n, 0);
+  EliminarMemoria(codigo, promedio);
+  cout << "==============================="<<endl;
+  cout << "Informacion adicional para monitorear como cambian los tamanios del array."<<endl;
+  cout << "Tamanio del array inicial: "<<n1<<endl;
+  cout << "Tamanio del array con los datos agregados: "<<n2<<endl;
+  cout << "Tamanio del array con los datos filtrados: "<<nf<<endl;
   return 0;
 }
