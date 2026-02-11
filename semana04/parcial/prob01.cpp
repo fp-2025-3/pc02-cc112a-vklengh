48585afe81922330e0925de6e13a42a0500fd854
Victor Cadillo <vklengh>
2026-02-11 02:05:33 +0000

    agregado mas salidas al arbol recursivo

--- semana04/parcial/prob01.cpp
+++ semana04/parcial/prob01.cpp
@@ -1,9 +1,10 @@
-#include <iostream>
 #include <climits>
+#include <iostream>
 
 using namespace std;
 
-int costoMaximoConSalidas(int arr[], int n, int pos, int cantLlamadas) {
+int costoMaximoConSalida(int arr[], int n, int pos, int arbol[][5], int &iter,
+                         int nivel) {
   // Caso base
   if (pos == n) {
     return 0;
@@ -13,10 +14,15 @@ int costoMaximoConSalidas(int arr[], int n, int pos, int cantLlamadas) {
   int producto = 1;
   for (int i = pos; i < n; i++) {
     producto *= arr[i];
-    int costoLocal = costoMaximoConSalidas(arr, n, i + 1, cantLlamadas + 1);
+    int costoLocal =
+        costoMaximoConSalida(arr, n, i + 1, arbol, iter, nivel + 1);
     int costoTotal = producto + costoLocal;
-    cout << "Llamada en rama: " << cantLlamadas
-         << " costo en ejecucion recursiva: " << costoTotal << endl;
+    arbol[iter][0] = pos;
+    arbol[iter][1] = i;
+    arbol[iter][2] = producto;
+    arbol[iter][3] = costoTotal;
+    arbol[iter][4] = nivel;
+    iter++;
     if (costoTotal > max)
       max = costoTotal;
   }
@@ -40,9 +46,10 @@ int costoMaximo(int arr[], int n, int pos) {
   }
   return max;
 }
-
 int main() {
   int arr[] = {1, 2, 3, 4, 5};
+  int arbol[100][5];
+  int iter = 0;
   int n = sizeof(arr) / sizeof(arr[0]);
   int cmin = costoMaximo(arr, n, 0);
   for (int i = 0; i < n; i++) {
@@ -50,7 +57,15 @@ int main() {
   }
   cout << endl;
   cout << "Costo maximo: " << cmin << endl;
-  cout << "Salidas de la recursion" << endl;
-  costoMaximoConSalidas(arr, n, 0, 0);
+  cout << "Salidas del arbol recursivo" << endl;
+  costoMaximoConSalida(arr, n, 0, arbol, iter, 0);
+  for (int i = iter - 1; i >= 0; i--) {
+    for (int d = 0; d < arbol[i][4]; d++) {
+      cout << "  ";
+    }
+    cout << "pos=" << arbol[i][0] << " || i=" << arbol[i][1]
+         << " || producto=" << arbol[i][2] << " || costo=" << arbol[i][3]
+         << endl;
+  }
   return 0;
 }
