f49fdeb4299c0c00561de3bb37b3b4e757875de1
Victor <vcadillog@uni.pe>
2026-02-13 17:36:51 -0500

    avance

--- semana05/practica04/prob04.cpp
+++ semana05/practica04/prob04.cpp
@@ -3,46 +3,51 @@
 using namespace std;
 
 struct Item {
-  char *descripcion;
+  // char *descripcion;
+  string descripcion;
   int cantidad;
   double PrecioUnitario;
 };
 
 struct Pedido {
   int numeroPedido;
-  char *nombreCliente;
+  // char *nombreCliente;
+  string nombreCliente;
   Item *items;
   int cantidadItems;
 };
 
-Item crearItem(const char *descripcion, int cantidad, double precio) {
-  Item item;
-  item.descripcion = new char;
-  strcpy(item.descripcion, descripcion);
-  item.PrecioUnitario = precio;
-  item.cantidad = cantidad;
-  return item;
+Item crearItem(string descripcion, int cantidad, double precio) {
+  Item *item = new Item;
+  // strcpy(item.descripcion, descripcion);
+  item->PrecioUnitario = precio;
+  item->cantidad = cantidad;
+  item->descripcion = descripcion;
+  return *item;
 }
 
-Pedido *crearPedido(int numero, const char *cliente, int cantidadItems) {
+Pedido *crearPedido(int numero, char *cliente, int cantidadItems) {
   Pedido *pedido = new Pedido;
   pedido->nombreCliente = new char;
-  Item *item = new Item;
   char *desc;
   int cant;
   double precio;
-  strcpy(pedido->nombreCliente, cliente);
+  // strcpy(pedido->nombreCliente, cliente);
+  pedido->nombreCliente = cliente;
   pedido->numeroPedido = numero;
   pedido->cantidadItems = cantidadItems;
-  desc = "Laptop\0";
+  desc = "Laptop";
   cant = 1;
   precio = 100;
-  *(*(pedido + 0)).items = crearItem(desc, cant, precio);
-  desc = "Mouse\0";
+  cout << 2<<endl;
+  *(pedido->items) = crearItem(desc, cant, precio);
+  // (*(pedido + 0))= crearItem(desc, cant, precio);
+  cout << 1<<endl;
+  desc = "Mouse";
   cant = 2;
   precio = 200;
   *(*(pedido + 1)).items = crearItem(desc, cant, precio);
-  desc = "Teclado\0";
+  desc = "Teclado";
   cant = 3;
   precio = 300;
   *(*(pedido + 2)).items = crearItem(desc, cant, precio);
